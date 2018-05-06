9da5d43c5b2b4cc096f26c67776e73838fe14d7b
matrix65537 <matrix65537@126.com>
2018-05-06 21:29:37 +0800

    AE -> daemon

--- src/c/AE/main.c
+++ src/c/AE/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "util.h"
+#include "time.h"
+
 #include <stdint.h>
 #include <stdlib.h>
 #include "ae.h"
@@ -125,6 +135,34 @@ void RWProc(struct aeEventLoop* eventLoop, int fd, void* clientData, int mask)
 	}
 }
 
+void go_daemon(void)
+{
+    pid_t pid;
+
+    pid = fork();
+    if(pid < 0)
+    {
+        perror("fork error");
+        exit(1);
+    }
+    else if(pid != 0)
+    {
+        exit(0);
+    }
+
+    setsid();
+
+    if(chdir("/") < 0)
+    {
+        perror("chdir error");
+        exit(1);
+    }
+    close(0);
+    open("/dev/null", O_RDWR);
+    dup2(0, 1);
+    dup2(0, 2);
+}
+
 extern void test_log();
 int main()
 {
@@ -134,6 +172,8 @@ int main()
 	int on = 1;
 	int r;
 	aeEventLoop* eventLoop = aeCreateEventLoop();
+
+    go_daemon();
 
 	log_set_level(LOG_LEVEL_DEVP);
 
